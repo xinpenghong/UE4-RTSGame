48a0fdcc9b85f79e37259f623e3bfd1d9e622501
Xinpeng HONG <44376097+Arthur-Hong@users.noreply.github.com>
2018-10-25 23:51:34 +0800

    Add files via upload

---
+++ MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class STEAMPROJECT_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
